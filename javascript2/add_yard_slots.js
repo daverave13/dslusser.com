f2dcf969b0f28a309f6d09a910998dd271ea416b
daverave13 <dslusser13@gmail.com>
2019-01-17 06:53:16 -0800

    added the plant dropdown back in

--- javascript2/add_yard_slots.js
+++ javascript2/add_yard_slots.js
@@ -1,5 +1,14 @@
 
-
+function assignPlantVar() {
+  $(".plant-btn").click(function(evt) {
+    //let plant = plant name here
+    console.log("success");
+    // let plant = $("#ABQ").html();
+    let plant = evt.target.id;
+    console.log(plant);
+  });
+}
 
 function funcStart() {
+  assignPlantVar();
 }
