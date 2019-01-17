4c111099e6600f048b97f7ba40f7441acc70ada4
daverave13 <dslusser13@gmail.com>
2019-01-17 06:33:05 -0800

    removed useless plant select dropdown

--- javascript2/add_yard_slots.js
+++ javascript2/add_yard_slots.js
@@ -1,14 +1,5 @@
 
-function assignPlantVar() {
-  $(".plant-btn").click(function(evt) {
-    //let plant = plant name here
-    console.log("success");
-    // let plant = $("#ABQ").html();
-    let plant = evt.target.id;
-    console.log(plant);
-  });
-}
+
 
 function funcStart() {
-  assignPlantVar();
 }
