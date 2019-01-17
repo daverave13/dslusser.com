7a2c84d5d2e8921131513c1090323744ee05d9ce
daverave13 <dslusser13@gmail.com>
2019-01-17 06:59:56 -0800

    changed button to blue

--- javascript2/add_yard_slots.js
+++ javascript2/add_yard_slots.js
@@ -1,9 +1,6 @@
 
 function assignPlantVar() {
   $(".plant-btn").click(function(evt) {
-    //let plant = plant name here
-    console.log("success");
-    // let plant = $("#ABQ").html();
     let plant = evt.target.id;
     console.log(plant);
   });
