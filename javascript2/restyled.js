70d34aeac0423de9fd57da2f436f928747a3dd0c
Dave <dslusser13@gmail.com>
2019-02-03 12:10:54 -0800

    autocommit lol

--- javascript2/restyled.js
+++ javascript2/restyled.js
@@ -2,6 +2,6 @@ const arr = ['1','2','3'];
 
 function populateLinks (arr) {
   for (x in arr) {
-    $("#links-target").append(`<h1>${arr[x]}</h1><br>`)
+    console.log(`<h1>${arr[x]}</h1><br>`);
   }
 }
