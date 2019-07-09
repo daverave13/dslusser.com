a02727b1583e19d425b498a55a05b1bb4c65232d
Dave <dslusser13@gmail.com>
2019-07-09 10:56:04 -0700

    auotcommit lol

--- javascript2/boot.js
+++ javascript2/boot.js
@@ -1,5 +1,8 @@
 // If user hasn't authed with Fitbit, redirect to Fitbit OAuth Implicit Grant Flow
 var fitbitAccessToken;
+const chart = require('chart.js');
+
+
 
 if (!window.location.hash) {
   window.location.replace('https://www.fitbit.com/oauth4/authorize?response_type=token&client_id=22DFML&redirect_uri=https%3A%2F%2Fwww.dslusser.com%2FEIT.html&scope=activity%20nutrition%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight');
@@ -31,6 +34,44 @@ async function fetchSteps() {
       for (let i = 30; i >= 24; i--) {
         if (stepLog[i].value > 0) $('#steps-list').append(`<li class='green'><h4>${stepLog[i].dateTime} | ${stepLog[i].value}</h4></li>`);
         if (stepLog[i].value == 0) $('#steps-list').append(`<li class='red'><h4>${stepLog[i].dateTime} | ${stepLog[i].value}</h4></li>`);
+
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
       }
     });
 }
