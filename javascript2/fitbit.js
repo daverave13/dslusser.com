bf76ff1429cd22325f75e49cea917337ce0f8a58
Dave <dslusser13@gmail.com>
2019-07-15 08:56:04 -0700

    auotcommit lol

--- javascript2/fitbit.js
+++ javascript2/fitbit.js
@@ -35,7 +35,7 @@ async function fetchSteps() {
         dateArr.push(stepLog[i].dateTime);
       }
 
-      let stepAvg = ([...stepArr].slice(1,6).map(x => parseInt(x,10))).reduce((a,b) => a+b,0)/6;
+      let stepAvg = ([...stepArr].slice(1,7).map(x => parseInt(x,10))).reduce((a,b) => a+b,0)/6;
       console.log([...stepArr].slice(1,6));
       if (stepAvg < 5000) {
         $('#step-readout').html(`Average steps/day: ${Math.floor(stepAvg)}. Get moving!`);
